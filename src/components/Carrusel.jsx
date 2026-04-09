7fd609922ff72a5140e3bcce4e4899ef4255d740
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-04-09 14:21:05 -0600

    Acomodamos mis tech stack

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -2,31 +2,38 @@ import { useEffect, useState } from "react";
 import "./Carrusel.css";
 
 const techs = [
-  { name: "Wix", image: "/icons/logos/wix.webp" },
-  { name: "HTML", image: "/icons/logos/HTML.webp" },
-  { name: "Figma", image: "/icons/logos/Figma.webp" },
-  { name: "CSS", image: "/icons/logos/CSS.webp" },
+  { name: "C+", image: "/icons/logos/cplusplus.webp" },
+  { name: "Python", image: "/icons/logos/python.webp" },
   { name: "JavaScript", image: "/icons/logos/JavaScrip.webp" },
   { name: "React", image: "/icons/logos/react.webp" },
   { name: "React Native", image: "/icons/logos/native.webp" },
+  { name: "CSS", image: "/icons/logos/CSS.webp" },
+  { name: "Tailwind css", image: "/icons/logos/tailwind.webp" },
+
+  { name: "HTML", image: "/icons/logos/HTML.webp" },
+  { name: "Figma", image: "/icons/logos/Figma.webp" },
+  { name: "Wix", image: "/icons/logos/wix.webp" },
+
+
   { name: "Stripe", image: "/icons/logos/stripe.webp" },
-  { name: "Vercel", image: "/icons/logos/vercel.webp" },
   { name: "Cloudflare", image: "/icons/logos/cloudflare.webp" },
-  { name: "Google Search Console", image: "/icons/logos/console.webp" },
+
+
   { name: "Google SEO", image: "/icons/logos/seo.webp" },
   { name: "Google Business Profile", image: "/icons/logos/business.webp" },
   { name: "Google Workspace", image: "/icons/logos/workspace.webp" },
   { name: "Google Workspace For Education", image: "/icons/logos/education.webp" },
-  { name: "C+", image: "/icons/logos/cplusplus.webp" },
-  { name: "Python", image: "/icons/logos/python.webp" },
+
   { name: "MATLAB", image: "/icons/logos/matlab.webp" },
   { name: "R", image: "/icons/logos/R.webp" },
   { name: "R Studio", image: "/icons/logos/R-Studio.webp" },
   { name: "Node.js", image: "/icons/logos/node.webp" },
-  { name: "Git", image: "/icons/logos/git.webp" },
-  { name: "SQL", image: "/icons/logos/SQL.webp" },
   { name: "MongoDB", image: "/icons/logos/MongoDB.webp" },
   { name: "MySQL", image: "/icons/logos/MySQL.webp" },
+  { name: "Google Search Console", image: "/icons/logos/console.webp" },
+  { name: "Vercel", image: "/icons/logos/vercel.webp" },
+  { name: "Git", image: "/icons/logos/git.webp" },
+  { name: "SQL", image: "/icons/logos/SQL.webp" },
 ];
 
 export default function TechCarousel() {
