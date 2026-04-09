3f146c5e9ec28ec26bc39611ad5ce29ffbc5fb4a
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-04-08 23:53:21 -0600

    Footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from "react-router-dom";
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 import Hero from './components/Hero.jsx';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <main>
             <Outlet /> {/* 👈 Aquí cambian las páginas */}
           </main>
+        <Footer/>
 
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className='container-footer'>
+        <div className='footer-left'>
+        © 2026 JOSE HUGO AGUAYO MENDOZA - PART OF 
+        <a 
+            href="https://www.nivostech.com/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="link-nivos"
+        >
+            NIVOSTECH
+        </a>
+        <span> - BUILT WITH PRECISION.</span>
+        </div>
+      <div className='footer-right'></div>
+        <div className="contenedor-botones">
+            <a
+            href="https://wa.me/523310392675?text=Hello%20Hugo%2C%20I%20saw%20your%20portfolio%20and%20I%E2%80%99m%20interested%20in%20speaking%20with%20you."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedln"
+            >
+            <img 
+            src="/icons/linkedin.webp" 
+            alt="WhatsApp" 
+            className="linkedln-icon"
+            />
+            LinkedIn
+            </a>
+            <a
+            href="https://wa.me/523310392675?text=Hello%20Hugo%2C%20I%20saw%20your%20portfolio%20and%20I%E2%80%99m%20interested%20in%20speaking%20with%20you."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="github"
+            >
+            <img 
+            src="/icons/github.webp" 
+            alt="WhatsApp" 
+            className="github-icon"
+            />
+            GitHub
+            </a>
+
+            <a
+            href="https://wa.me/523310392675?text=Hello%20Hugo%2C%20I%20saw%20your%20portfolio%20and%20I%E2%80%99m%20interested%20in%20speaking%20with%20you."
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp"
+            >
+            <img 
+            src="/icons/WhatsApp.webp" 
+            alt="WhatsApp" 
+            className="whatsapp-icon"
+            />
+            WhatsApp
+            </a>
+        </div>
+      </div>
+  )
+}
+
+export default Footer
