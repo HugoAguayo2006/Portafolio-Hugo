e1a6018f15ce34b6c1960a219090bbaedb767aff
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-04-08 22:31:26 -0600

    Navbar Terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router";
 import Hero from "../components/Hero";
+import "./Home.css";
 const Home = () => {
     return(
         <>
+        <div className="home">
             <h1>Home</h1>
             <p>
                 <Link to="/"> Go back</Link>
             </p>
             
+        </div>
+
+
             <Hero/>
 
         </>
