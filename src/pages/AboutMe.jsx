4449b8bf59df09541bf3785bdaf881d37688f239
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-04-09 18:52:40 -0600

    Estructura del About me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,14 +1,49 @@
 import { Link } from "react-router";
+import "./AboutMe.css"
+import TechCarousel from "../components/Carrusel";
 
 const AboutMe = () =>{
     return(
-        <>
-            <h1>About me</h1>
-            <p>
-                <Link to="/"> Go back</Link>
-            </p>
-        </>
+    <>
+        <div className="aboutme-page">
+
+            <section className="aboutme-container">
+                <div className="aboutme-top">
+                    <p className="letras-pequenas-arriba-centrado">
+                        /* Jose Hugo Aguayo Mendoza */
+                    </p>
+                    <h1 className="aboutme-title-text">
+                        This is <span className='secondary-text'>Me</span>
+                    </h1>
+                </div>
+
+                <div className="contactMe-bottom">
+                    {/* COLUMNA IZQUIERDA */}
+
+
+                    {/* COLUMNA DERECHA */}
+                    <div className="contact-right">
+
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            <section>
+
+                CITA
+            </section>
+
+            <div className="line"></div>
+                <TechCarousel /> 
+            <div className="line"></div>
+
+            <p>CERTIFICADOS</p>
+        </div>
+    </>
     );
-};
+}
 
 export default AboutMe
